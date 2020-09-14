e7209b5b18ad7c256c4012a8fa0464d1f00f5b4c
NorthStarFan <NorthStarFan@users.noreply.github.com>
2020-09-13 22:13:43 -0500

    Part1Lab2
    My class schedule printed out

--- c++Lab2/main.cpp
+++ c++Lab2/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  c++Lab2
-//
-//  Created by Justin Stuelke on 9/1/20.
-//  Copyright © 2020 Justin Stuelke. All rights reserved.
-//
+// Justin Stuelke
+// Lab 2 Exploring Output
+// Due 9/14/2020
+// c++ fall semester 2020
+
 
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()    // main program
+
+
+
+{
+    const string Tuesday = "Tuesday"; // create varible for tuesday
+    const string P = "Python"; // create varible for Python
+    const string C = "C++"; // create varible c++
+    const string T = "9:30-11:00"; // create varible for time for python 
+    
+
+    
+    cout << "Monday 1:30-2:30 " << C << endl;
+    
+    cout << Tuesday<<" "<< T <<" "<< P << endl;
+    
+    cout << Tuesday<<" " "1:00-2:30 " << C << endl;
+    
+    cout << "Thursday "<< T <<" "<< P << endl;
+    
     return 0;
 }
+
+
+
